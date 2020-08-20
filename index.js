723316f9a5f92a44c954b79d5c5502d61aa4d21e
VAIBHAV PRATAP SINGH <vaibhav1bab@gmail.com>
2020-08-20 08:53:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+//Don't Remove try and catch block
+//Write your code in try block
+//vaibhavpratapsingh.com
 try{
-
-    function helllo()
+    let a = 5;
+    let b = 5;
+    let c = a+b;
+    cout(c); //use cout instead of console.log(); 
+    
+    for(let i = 0;i<10;i++)
     {
-        cout("hello");
+        cout("@_iamvps_");
     }
     
-
-
-
+    try{
+        let divide = 10/0;
+    }
+    catch(e)
+    {
+        cout(e);   
+    }
+    
+    
+    
 }catch(err)
 {
     cout(err.message);
